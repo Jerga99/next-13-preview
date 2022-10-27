2d0ff1d5c41fd4894020e75412b849b07096ce40
Filip Jerga <jerga99@gmail.com>
2022-10-28 07:07:07 +0800

    More routes

---
+++ app/blogs/page.jsx
@@ -0,0 +1,10 @@
+
+
+
+export default function BlogsPage() {
+  return (
+    <div className="page">
+      <h1>Hi there: Blogs Page</h1>
+    </div>
+  )
+}
---
+++ app/products/page.jsx
@@ -0,0 +1,10 @@
+
+
+
+export default function ProductsPage() {
+  return (
+    <div className="page">
+      <h1>Hi there: Products Page</h1>
+    </div>
+  )
+}
