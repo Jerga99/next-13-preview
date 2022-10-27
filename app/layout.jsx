85f02e2ce4bbade212277d5cb77d296f24bd7cfd
Filip Jerga <jerga99@gmail.com>
2022-10-28 07:01:49 +0800

    Pages

---
+++ app/layout.jsx
@@ -0,0 +1,11 @@
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <head></head>
+      <body>
+        Layout Code
+        {children}
+      </body>
+    </html>
+  )
+}
