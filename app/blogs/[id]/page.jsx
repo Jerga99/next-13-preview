d04ecfbbab1508eed754908c7ad90cc0095936b4
Filip Jerga <jerga99@gmail.com>
2022-10-28 07:09:02 +0800

    Detail pages

---
+++ app/blogs/[id]/page.jsx
@@ -0,0 +1,10 @@
+
+
+
+export default function BlogDetailPage({params}) {
+  return (
+    <div className="page">
+      <h1>Hi there Blog: {params.id}</h1>
+    </div>
+  )
+}
---
+++ app/products/[slug]/page.jsx
@@ -0,0 +1,10 @@
+
+
+
+export default function ProductDetailPage({params}) {
+  return (
+    <div className="page">
+      <h1>Hi there Product: {params.slug}</h1>
+    </div>
+  )
+}
